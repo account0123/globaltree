73f7dc87a31a3c6261d64681847918d111d38fcf
Ian Parra <ianparra200@gmail.com>
2025-05-16 01:09:35 -0400

    feat: cors configuration allow localhost

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,20 @@
+import { CorsOptions } from "cors";
+
 const {
   PORT = 3000,
   DB_URI = "mongodb://localhost:27017/linktree",
 } = process.env;
 
-export { PORT, DB_URI };
+const allowedOrigins: string[] = [];
+
+const corsOptions: CorsOptions = {
+  origin(requestOrigin, callback) {
+    if (requestOrigin && (allowedOrigins.includes(requestOrigin) || requestOrigin.startsWith("http://localhost:"))) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+}
+
+export { PORT, DB_URI, corsOptions };
--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import root from "./root.routes.js";
 import auth from "./auth.routes.js";
+import { corsOptions } from "../../config.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use("/", root);
 app.use("/auth", auth);
