6fe5fb35b7fc7d8deb857aca65bc4c617dd3f325
Ian Parra <ianparra200@gmail.com>
2025-05-17 22:29:20 -0400

    fix: allow undefined (local) origins only in development

--- src/config.ts
+++ src/config.ts
@@ -4,18 +4,22 @@ const {
   PORT = 3000,
   DB_URI = "mongodb://localhost:27017/linktree",
   PUBLIC_WEB_URL = "http://localhost",
+  NODE_ENV = "development",
 } = process.env;
 
-const allowedOrigins: string[] = [PUBLIC_WEB_URL];
+const allowedOrigins: (string | undefined)[] = [PUBLIC_WEB_URL];
 
 const corsOptions: CorsOptions = {
   origin(requestOrigin, callback) {
-    if (!requestOrigin || (allowedOrigins.includes(requestOrigin) || requestOrigin.startsWith("http://localhost:"))) {
+    if (
+      (!requestOrigin && NODE_ENV == "development") ||
+      (requestOrigin && allowedOrigins.includes(requestOrigin))
+    ) {
       callback(null, true);
     } else {
       callback(new Error(`Origin ${requestOrigin} not allowed by CORS`));
     }
   },
-}
+};
 
 export { PORT, DB_URI, corsOptions };
