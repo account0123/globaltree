ddc406c18f3b5e4a2607facd08c0ef137432ba2e
Ian Parra <ianparra200@gmail.com>
2025-05-25 19:04:12 -0400

    fix: /users routes dont mix up with root (/) route

--- src/infrastructure/http/users.routes.ts
+++ src/infrastructure/http/users.routes.ts
@@ -1,9 +1,12 @@
+import { Router } from "express";
 import { editUser, getSelfUser, setSelfUserAvatar } from "./controllers/user.controller.js";
+import { authorization } from "./middlewares/auth.middleware.js";
 import { logRequest } from "./middlewares/log.middleware.js";
-import router from "./root.routes.js";
 
-router.get("/@me", getSelfUser);
-router.patch("/@me", logRequest, editUser);
-router.put("/@me/avatar", setSelfUserAvatar);
+const router = Router();
+
+router.get("/@me", authorization, getSelfUser);
+router.patch("/@me", logRequest, authorization, editUser);
+router.put("/@me/avatar", authorization, setSelfUserAvatar);
 
 export default router;
