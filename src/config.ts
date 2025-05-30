acf20ce79dfcbd9b4ea47a1bb311b04a1fad0b3c
Ian Parra <ianparra200@gmail.com>
2025-05-29 22:37:40 -0400

    fix: allow local origin on production

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,6 @@ const {
   PORT = 3000,
   DB_URI = "mongodb://localhost:27017/linktree",
   PUBLIC_WEB_URL = "http://localhost",
-  NODE_ENV = "development",
 } = process.env;
 
 const allowedOrigins: (string | undefined)[] = [PUBLIC_WEB_URL];
@@ -12,7 +11,7 @@ const allowedOrigins: (string | undefined)[] = [PUBLIC_WEB_URL];
 const corsOptions: CorsOptions = {
   origin(requestOrigin, callback) {
     if (
-      (!requestOrigin && NODE_ENV == "development") ||
+      (!requestOrigin) ||
       (requestOrigin && allowedOrigins.includes(requestOrigin))
     ) {
       callback(null, true);
